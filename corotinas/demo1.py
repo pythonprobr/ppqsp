7ab5117546dae96378a7177ad5e74a2df07159ec
Luciano Ramalho <luciano@ramalho.org>
2013-12-08 11:55:09 -0200

    demos simples com corrotinas

---
+++ corotinas/demo1.py
@@ -0,0 +1,25 @@
+def corrotina():
+    print('\t(corrotina) inciciando...')
+    x = yield 1
+    print('\t(corrotina) recebeu x: %r' % x)
+    y = yield 2
+    print('\t(corrotina) recebeu y: %r' % y)
+    print('\t(corrotina) terminando.')
+
+
+def principal():
+    print('(principal) iniciando...')
+    co = corrotina()
+    print('(principal) corrotina instanciada: %r' % co)
+    res = next(co)
+    print('(principal) produzido por next(co): %r' % res)
+    res2 = co.send(88)
+    print('(principal) produzido por co.send(88): %r' % res2)
+    try:
+        res3 = co.send(99)
+        # o print a seguir nunca vai acontecer
+        print('(principal) produzido por co.send(99): %r' % res3)
+    except StopIteration:
+        print('(principal) a corotina nao tem mais valores a produzir')
+principal()
+
